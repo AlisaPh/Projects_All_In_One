b34880e425ba66fb20a299b9cff9d891c4289f0d
Alisa <a.arthopkins@gmail.com>
2020-12-21 22:20:33 +0200

    datetime finished!

--- date_difference.py
+++ date_difference.py
@@ -1,7 +1,11 @@
+from math import fabs
 from datetime import datetime
 date= datetime.now()
-month_now=date.strftime("%m")
+month_now=int(date.strftime("%m"))
 year_now=datetime.today().year
 from random import randint
 rnd_year=randint(1980,year_now)
 rnd_month=randint(1,12)
+sum_years=year_now-rnd_year
+sum_months=int(fabs(month_now-rnd_month))
+print(sum_months, "/", sum_years)
