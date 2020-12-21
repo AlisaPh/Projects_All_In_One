cdc79d5e781b9270279bacd184e6a6278380f5e7
Alisa <a.arthopkins@gmail.com>
2020-12-21 19:24:49 +0200

    inr(num)

--- digit_in_number.py
+++ digit_in_number.py
@@ -1,6 +1,6 @@
-num=int(input("Enter a number between 1 to 99: "))
-digit=int(input("Enter a number: "))
-if(num>99 or num<1):
+num=input("Enter a number between 1 to 99: ")
+digit=input("Enter a number: ")
+if(int(num)>99 or int(num)<1):
     print("EROR")
 else:
     print(num.count(digit))
