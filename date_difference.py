9620d111629364ccfff515a8669bb0b6ffef8b7c
Alisa <a.arthopkins@gmail.com>
2020-12-21 21:57:21 +0200

    datetime rnd

--- date_difference.py
+++ date_difference.py
@@ -2,3 +2,6 @@ from datetime import datetime
 date= datetime.now()
 month_now=date.strftime("%m")
 year_now=datetime.today().year
+from random import randint
+rnd_year=randint(1980,year_now)
+rnd_month=randint(1,12)
