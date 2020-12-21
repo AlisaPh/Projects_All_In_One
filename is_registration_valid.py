71aa11c6626c80a74b07b3abf53b748e476e87c1
Alisa <a.arthopkins@gmail.com>
2020-12-21 19:36:33 +0200

    is_registration_valid completed!

--- is_registration_valid.py
+++ is_registration_valid.py
@@ -1,2 +1,9 @@
 name = input("Enter your username: ")
-password = input("Enter your password: ")
+if("@" in name and "." in name):
+    password = input("Enter your password: ")
+    if("#" in password and len(password)>=8):
+        print("Congratulations! You did it!")
+    else:
+        print("Invalid Password")
+else:
+    print("Invalid Username")
